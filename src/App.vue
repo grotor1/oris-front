bec13ddfe60cfe48b337b112a4c84512c9af3559
vladimir <varukhin04@mail.ru>
2024-03-04 08:30:06 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Counter from "@/components/Counter.vue";
+import {ref, watch} from "vue";
+
+const updateHandler = (value) => alert(value);
+
+const count = ref(0);
+
+watch(count, (value) => {
+  console.log(value, 'change');
+})
+</script>
+
+<template>
+  <Counter v-model="count">
+    <template #label="{divider}">
+      Бананы {{divider}}
+    </template>
+  </Counter>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,67 @@
+<script setup>
+import {computed, onBeforeMount, onBeforeUnmount, onMounted, onUpdated, ref, watch} from "vue";
+
+const props = defineProps({
+  modelValue: Number,
+  initial: {
+    type: Number,
+    default: 4,
+  }, // String, Object, Function
+});
+
+const emits = defineEmits(['update:modelValue']);
+
+const count = ref(props.initial);
+
+const double = computed(() => count.value * 2);
+
+watch(count, (value) => {
+  emits('update:modelValue', value);
+})
+
+watch(() => props.modelValue, (value) => {
+  count.value = value;
+})
+</script>
+
+<template>
+  <div class="container">
+    <button @click="count += 1">
+      +
+    </button>
+
+    <span>
+      <slot name="label" divider=":"/> {{ count }}
+    </span>
+
+    <button @click="count -= 1">
+      -
+    </button>
+
+    <input v-model="count"
+           type="number">
+
+    {{ double }}
+
+    {{ initial }}
+
+    <span v-for="item in count">
+      {{item}}
+    </span>
+
+    <span v-if="count !== 0">
+      Бананчики
+    </span>
+
+    <span v-else>
+     Нет бананчиков
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  gap: 8px
+}
+</style>
